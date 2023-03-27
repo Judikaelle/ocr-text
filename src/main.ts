31ecc5a1037cc15f837213e66f4cc982cd813fde
Pierre <pierre.gicquel@protonmail.ch>
2023-03-27 12:00:19 +0200

    Fix upload file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {assignColors, resetFiles} from "./utils";
 import {createCharactersButtons, createResetButton} from "./html";
-import {charactersDiv, fileInput, fileList, loadingDiv, resultSection} from "./dom";
+import {charactersDiv, fileInput, loadingDiv, resultSection} from "./dom";
 import {addFileToDB, getFileFromDB} from "./database";
 import {importExcel} from "./import";
 import {dialoguesToHtml, formatExcelFile, selectedCharacter} from "./dialogue";
@@ -32,15 +32,7 @@ fileInput.addEventListener('change', async () => {
     file = fileInput.files?.[0];
     if (file) {
         addFileToDB("1", file).then(r => console.log(r))
-        resetFiles(fileInput, resultSection, charactersDiv);
-        fileList.innerText = file.name;
-        loadingDiv.innerHTML = 'Working...';
-        importExcel(file).then(r => console.log(r))
-    }
-
-    // Affichage du résultat dans la div prévue à cet effet
-    if (resultSection && excelText) {
-        resultSection.innerHTML = '';
+        excelText = await importExcel(file)
         let {allDialogue, allCharacters} = formatExcelFile(excelText)
         dialoguesToHtml(allDialogue, resultSection, allCharacters, characterColors);
         loadingDiv.innerHTML = '';
